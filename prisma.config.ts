57858384f7224517ba91d5bccfdfbb4e7fee3b20
votantai3070 <votantai.07092002@gmail.com>
2026-07-07 00:29:39 +0700

    fix(prisma): 6.19.3 & generate

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,12 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("Missing DATABASE_URL");
-}
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
   datasource: {
-    url: process.env.DATABASE_URL,
+    url:process.env["DATABASE_URL"]!,
   },
 });
--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,15 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
 
-const { Pool } = pg;
+const globalForPrisma = globalThis;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+  });
 
-const adapter = new PrismaPg(pool);
-
-export const prisma = new PrismaClient({
-  adapter,
-});
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
